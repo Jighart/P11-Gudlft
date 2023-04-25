cd2d755510dbad7eeab46f518ad36396f3b0e5fb
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-04-25 14:51:46 +0200

    Adding locust performance test

---
+++ tests/performance_tests/locustfile.py
@@ -0,0 +1,46 @@
+import json
+from locust import HttpUser, task, between
+
+def loadClubs():
+    with open('../../clubs.json') as c:
+         listOfClubs = json.load(c)['clubs']
+         return listOfClubs
+
+
+def loadCompetitions():
+    with open('../../competitions.json') as comps:
+         listOfCompetitions = json.load(comps)['competitions']
+         return listOfCompetitions
+
+
+class LocustTestServer(HttpUser):
+    wait_time = between(1, 5)
+    competition = loadCompetitions()[0]
+    club = loadClubs()[0]
+
+    def on_start(self):
+        self.client.get("/", name=".index")
+        self.client.post("/showSummary", data={'email': self.club["email"]}, name=".show_summary")
+
+    @task
+    def get_booking(self):
+        self.client.get(
+            f"/book/{self.competition['name']}/{self.club['name']}",
+            name="book"
+        )
+
+    @task
+    def post_booking(self):
+        self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 0,
+                "club": self.club["name"],
+                "competition": self.competition["name"]
+            },
+            name="purchase_places"
+        )
+
+    @task
+    def get_board(self):
+        self.client.get("/showPointBoard", name="view_clubs")
